8c6dcaa3ac73446088b40e552852a481543da40f
Bartha Hunor <bhun0r97@gmail.com>
2021-10-15 12:24:06 +0200

    animation added

--- random_quote_machine/src/components/Body.js
+++ random_quote_machine/src/components/Body.js
@@ -1,18 +1,58 @@
 import React from "react";
+import $ from "jquery";
+import { findDOMNode } from "react-dom";
 import { useState, useEffect } from "react";
 import style from "../style/style.css";
 import resets from "../style/resets.css";
 
 export default function Body() {
+  let bgColor = [
+    "#16a085",
+    "#27ae60",
+    "#2c3e50",
+    "#f39c12",
+    "#e74c3c",
+    "#9b59b6",
+    "#FB6964",
+    "#342224",
+    "#472E32",
+    "#BDBB99",
+    "#77B1A9",
+    "#73A857",
+  ];
+
+  // random bg
+  const randomBg = Math.floor(Math.random() * bgColor.length);
+
   const [currentQuote, setQuote] = useState("");
 
   const getQuote = () => {
     // random number
     let randomNumber = Math.floor(Math.random() * 100);
+
     // fetch
     fetch("https://type.fit/api/quotes")
       .then((res) => res.json())
       .then((data) => setQuote(data[randomNumber]));
+
+    let textElement = document.getElementById("text");
+    $(textElement).animate({ opacity: 0 }, 0, function () {
+      $(this).animate({ opacity: 1 }, 800);
+    });
+
+    let authorElement = document.getElementById("author");
+    $(authorElement).animate({ opacity: 0 }, 0, function () {
+      $(this).animate({ opacity: 1 }, 800);
+    });
+
+    let sectionElement = document.getElementById("quote-box");
+    $(sectionElement).animate({ opacity: 0 }, 0, function () {
+      $(this).animate({ opacity: 1 }, 1000);
+    });
+  };
+
+  const background = {
+    backgroundColor: bgColor[randomBg],
   };
 
   // Use effect
@@ -22,7 +62,7 @@ export default function Body() {
 
   return (
     <div>
-      <section id="quote-box">
+      <section style={background} id="quote-box">
         <i class="fas fa-quote-left"></i>
         <div id="text">{currentQuote.text}</div>
         <div id="author">- {currentQuote.author}</div>
