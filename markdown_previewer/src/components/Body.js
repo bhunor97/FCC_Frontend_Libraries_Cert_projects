8a5e5571630819a8d3ba75a71778d3255a3302ee
Bartha Hunor <bhun0r97@gmail.com>
2021-10-15 13:22:22 +0200

    text-copy funciton added

--- markdown_previewer/src/components/Body.js
+++ markdown_previewer/src/components/Body.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-    return (
-        <div>
-            <h1>Hello react</h1>
+  const [currentText, setText] = useState("");
+
+  const textUpdateFunc = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      {/* Editor */}
+      <section className="editor-section">
+        <textarea
+          onChange={textUpdateFunc}
+          id="editor"
+          name=""
+          cols="30"
+          rows="10"
+          placeholder="Text Area Right Here"
+        ></textarea>
+      </section>
+      {/* Preview */}
+      <section className="preview-section">
+        <div id="preview" className="preview">
+          {currentText}
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
 export default Body;
