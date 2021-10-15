fb276f6ab1478a8e94704998a329ec88c216101c
Bartha Hunor <bhun0r97@gmail.com>
2021-10-15 19:48:58 +0200

    requirements passed - no stlyes yet

--- markdown_previewer/src/components/Body.js
+++ markdown_previewer/src/components/Body.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import marked from "marked";
+import style from "./style/style.css";
 
 const Body = () => {
   const [currentText, setText] = useState("");
@@ -8,6 +9,53 @@ const Body = () => {
   const textUpdateFunc = (event) => {
     setText(event.target.value);
   };
+
+  let plcHolder = ` 
+  # Largest Heading
+  ## Second Largest
+  ### Heading Three
+  #### Heading Four
+  ##### Heading Five
+  ###### Heading Six
+
+  [Personal Portfolio Bartha Hunor](https://bhunor97.github.io/Portfolio/)
+
+  \`\`\`
+  javascript
+  function add(num1, num2) {
+      return num1 + num2
+  }
+  \`\`\` 
+
+  *This text is italic*
+
+  _This is also italic_
+
+  **This is bold**
+
+  __This is also bold__
+
+  ~~This text is strike through~~
+
+
+  > This is a quote
+
+  * item 1
+* item 2
+* item 3
+* item 4
+    * nested item 1
+    * nested item 2
+    * nested item 3
+  
+  \`<p>This is a paragraph</p>\`
+
+  ![random_picture](https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/1200px-Markdown-mark.svg.png)
+  `;
+
+  useEffect(() => {
+    setText(plcHolder);
+  }, []);
 
   return (
     <div>
@@ -19,8 +67,9 @@ const Body = () => {
           name=""
           cols="30"
           rows="10"
-          placeholder="Text Area Right Here asd"
-        ></textarea>
+        >
+          {plcHolder}
+        </textarea>
       </section>
       {/* Preview */}
       <section className="preview-section">
