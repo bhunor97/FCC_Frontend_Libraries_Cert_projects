b7884e8d968e8ec66264fa21095e3ee091b65375
Bartha Hunor <bhun0r97@gmail.com>
2021-10-18 13:01:00 +0200

    drum machine - initial commit

---
+++ drum_machine/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/style.css";
+import "../style/resets.css";
+
+const Body = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+export default Body;
