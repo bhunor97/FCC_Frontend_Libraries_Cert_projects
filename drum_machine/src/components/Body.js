ede1e9e84249a525c4536a627bf36c8f304bc863
Bartha Hunor <bhun0r97@gmail.com>
2021-10-18 17:21:53 +0200

    sounds imported

--- drum_machine/src/components/Body.js
+++ drum_machine/src/components/Body.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "../style/style.css";
 import "../style/resets.css";
-import hihat from "../audio/1_2_3.wav";
+import clap_1 from "../audio/Clap-1-short.wav";
+import clap_2 from "../audio/Clap-2-short.wav";
+import hat_1 from "../audio/Hat-long-1.wav";
+import hat_2 from "../audio/Hat-short-1.wav";
+import hat_3 from "../audio/Hat-short-2.wav";
+import kick_1 from "../audio/Kick-short-1.wav";
+import kick_2 from "../audio/Kick-short-2.wav";
+import kick_3 from "../audio/Kick-short-3.wav";
+import snare_1 from "../audio/Snare-1-short.wav";
 
 const Body = () => {
   return (
     <div className="container">
       <audio controls>
-        <source src={hihat} type="audio/ogg" />
+        <source src={clap_1} type="audio/ogg" />
         Your browser does not support the audio element.
       </audio>
     </div>
