9640230f670962515492b4d15d81317b7a17e03f
Bartha Hunor <bhun0r97@gmail.com>
2021-10-18 16:57:25 +0200

    audio test fix

--- drum_machine/src/components/Body.js
+++ drum_machine/src/components/Body.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../style/style.css";
 import "../style/resets.css";
+import hihat from "../audio/1_2_3.wav";
 
 const Body = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="container">
+      <audio controls>
+        <source src={hihat} type="audio/ogg" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
   );
 };
