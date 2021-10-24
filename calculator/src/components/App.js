db41d37d050fba39008d7243aa5df73d8e27c78b
Bartha Hunor <bhun0r97@gmail.com>
2021-10-24 13:38:49 +0200

    calculator - initial commit

---
+++ calculator/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../style/style.css";
+import resets from "../style/resets.css";
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
