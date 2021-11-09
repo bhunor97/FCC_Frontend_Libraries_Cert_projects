d540e6c5c9a8c664183604d4145c9b900608854e
Bartha Hunor <bhun0r97@gmail.com>
2021-11-09 17:18:11 +0100

    10/29 passed

--- clock/src/components/App.js
+++ clock/src/components/App.js
@@ -3,8 +3,37 @@ import { useState, useEffect } from "react";
 import "./style.css";
 
 function App() {
+  // states
   const [currentBreakLenght, setBreakLength] = useState(5);
   const [currentSeshLength, setSeshLength] = useState(25);
+  const [currentDisplayLength, setDisplayLength] = useState(currentSeshLength);
+
+  useEffect(() => {
+    setBreakLength(5);
+    setSeshLength(25);
+  }, []);
+
+  // Reset function
+  const resetFunc = () => {
+    setBreakLength(5);
+    setSeshLength(25);
+  };
+
+  // Break Increment / Decrement functions
+  const incrementBreak = () => {
+    setBreakLength(currentBreakLenght + 1);
+  };
+  const decrementBreak = () => {
+    setBreakLength(currentBreakLenght - 1);
+  };
+
+  // Session Increment / Decrement
+  const incrementSesh = () => {
+    setSeshLength(currentSeshLength + 1);
+  };
+  const decrementSesh = () => {
+    setSeshLength(currentSeshLength - 1);
+  };
 
   return (
     <div className="App">
@@ -12,30 +41,40 @@ function App() {
       <div className="break-session-container">
         {/* Break Length Section */}
         <section id="break-label">
-          <h3 className="#">Break Length</h3>
+          <h3>Break Length</h3>
           <div className="btn-display-container">
-            <button id="break-decrement">decr-btn</button>
+            <button id="break-decrement" onClick={incrementBreak}>
+              +break
+            </button>
             <div id="break-length">{currentBreakLenght}</div>
-            <button id="break-increment">incr-btn</button>
+            <button id="break-increment" onClick={decrementBreak}>
+              -break
+            </button>
           </div>
         </section>
         {/* Session Length Section */}
         <section id="session-label">
-          <h3 className="#">Session Length</h3>
+          <h3>Session Length</h3>
           <div className="btn-display-container">
-            <button id="session-decrement">decr-btn</button>
+            <button id="session-decrement" onClick={incrementSesh}>
+              +sesh
+            </button>
             <div id="session-length">{currentSeshLength}</div>
-            <button id="session-increment">incr-btn</button>
+            <button id="session-increment" onClick={decrementSesh}>
+              -sesh
+            </button>
           </div>
         </section>
       </div>
       {/* Timer Section */}
       <section id="timer-label">
-        <h2 className="#">Session</h2>
-        <div id="time-left">time left display</div>
+        <h2>Session</h2>
+        <div id="time-left">{currentDisplayLength}</div>
         <div className="btn-display-container">
-          <button id="start-stop">start-stop-btn</button>
-          <button id="reset">reset-btn</button>
+          <button id="start_stop">start-stop-btn</button>
+          <button id="reset" onClick={resetFunc}>
+            reset-btn
+          </button>
         </div>
       </section>
     </div>
@@ -43,3 +82,6 @@ function App() {
 }
 
 export default App;
+
+// User Story #8
+// User Story #11
